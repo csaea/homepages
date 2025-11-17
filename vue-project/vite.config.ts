7ac5b426bfa7374d52de99d985a35b6dc20c7c17
Computer Science and Applied Engineering Academy <mharrell@hcvsd.org>
2025-11-17 19:33:36 +0000

    updates deploy data

--- vue-project/vite.config.ts
+++ vue-project/vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     vueJsx(),
     vueDevTools(),
   ],
+  base: '/homepages/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
